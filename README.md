2dfb159da0305a9f83f78e2157f0b992b624b9aa
vilmal <vilma.liorensaityte@gmail.com>
2023-03-23 22:25:27 +0100

    Clean up theme

--- README.md
+++ README.md
@@ -1,16 +1,29 @@
 # APEX Theme
 APEX is the new bootstrap 5 based OXID Theme for twig engine
 
-## installation
-Use composer to install theme
+## Installation
+
 The theme is compatible with e-Shop b-7.0.x branch
+Run the following command to install theme:
 
 ```bash
 composer config repositories.oxid-esales/apex-theme vcs https://github.com/OXID-eSales/apex-theme
 composer require oxid-esales/apex-theme:dev-main
 ```
 
-or simply clone the repository
+## Activation
+
+Open OXID eShop administration panel and go to:`Extensions --> Themes`,
+select a Twig theme and click activation button.
 
 ## Usage
+
 For development you will need npm installed https://nodejs.org/ and grunt for the built tasks https://gruntjs.com/
+
+## License
+
+See LICENSE file for license details.
+
+## Bugs and Issues
+
+If you experience any bugs or issues, please report them in the section **OXID eShop (all versions)** under category **Twig engine** of https://bugs.oxid-esales.com
--- theme.php
+++ theme.php
@@ -1,227 +1,235 @@
 <?php
-$aTheme = array(
-    'id' => 'APEX',
+
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+declare(strict_types=1);
+
+$aTheme = [
+    'id' => 'apex',
     'title' => 'APEX Theme',
     'description' => 'APEX - Bootstrap 5 TWIG Theme',
     'thumbnail' => 'apex_green.svg',
     'version' => '1.1.0',
     'author' => 'Tino Favetto, c&c concepts and creations GmbH',
-    'settings' => array(
+    'settings' => [
 //DISPLAY
-        array(
+        [
             'group' => 'display',
             'name' => 'aNrofCatArticles',
             'type' => 'arr',
-            'value' => array("20"),
-        ),
-        array(
+            'value' => ["20"]
+        ],
+        [
             'group' => 'display',
             'name' => 'aNrofCatArticlesInGrid',
             'type' => 'arr',
-            'value' => array("20"),
-        ),
-        array(
+            'value' => ["20"],
+        ],
+        [
             'group' => 'display',
             'name' => 'blShowBirthdayFields',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'display',
             'name' => 'blShowFinalStep',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'display',
             'name' => 'blShowListDisplayType',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'display',
             'name' => 'blShowWeightInList',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'display',
             'name' => 'iNewBasketItemMessage',
             'type' => 'select',
             'value' => 'str',
             'constraints' => '0|1|2|3',
-        ),
-        array(
+        ],
+        [
             'group' => 'display',
             'name' => 'sDefaultListDisplayType',
             'type' => 'select',
             'value' => 'grid',
             'constraints' => 'line|grid',
-        ),
+        ],
 
 //STARTPAGE
 
-        array(
+        [
             'group' => 'startpage',
             'name' => 'showCategoryTiles',
             'type' => 'bool',
             'value' => 1,
             'position' => 21,
-        ),
-        array(
+        ],
+        [
             'group' => 'startpage',
             'name' => 'bl_showManufacturer',
             'type' => 'bool',
             'value' => 1,
             'position' => 30,
-        ),
-        array(
+        ],
+        [
             'group' => 'startpage',
             'name' => 'sShowBargainArticles',
             'type' => 'bool',
             'value' => 1,
             'position' => 35,
-        ),
-        array(
+        ],
+        [
             'group' => 'startpage',
             'name' => 'sShowNewestArticles',
             'type' => 'bool',
             'value' => 1,
             'position' => 40,
-        ),
-        array(
+        ],
+        [
             'group' => 'startpage',
             'name' => 'sShowTopArticles',
             'type' => 'bool',
             'value' => 1,
             'position' => 50,
-        ),
+        ],
 //DETAIL
-        array(
+        [
             'group' => 'pdp',
             'name' => 'sProductListNavigation',
             'type' => 'bool',
             'value' => 0,
             'position' => 40,
-        ),
+        ],
 
 //KATEGORIEN
 
-        array(
+        [
             'group' => 'pop',
             'name' => 'sShowPopBreadcrump',
             'type' => 'bool',
             'value' => 1,
-        ),
+        ],
 
 //FUNKTIONEN
-        array(
+        [
             'group' => 'features',
             'name' => 'bl_showCompareList',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'features',
             'name' => 'bl_showGiftWrapping',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'features',
             'name' => 'bl_showVouchers',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'features',
             'name' => 'bl_showWishlist',
             'type' => 'bool',
             'value' => 1,
-        ),
+        ],
 
 //E-MAIL
-        array(
+        [
             'group' => 'emails',
             'name' => 'blEmailsShowProductPictures',
             'type' => 'bool',
             'value' => 0,
-        ),
+        ],
 
 //FOOTER
-        array(
+        [
             'group' => 'footer',
             'name' => 'blFooterShowHelp',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'blFooterShowLinks',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'blFooterShowNewsletter',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'blFooterShowNewsletterForm',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sBlogUrl',
             'type' => 'str',
             'value' => 'https://wordpress.org',
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sFacebookUrl',
             'type' => 'str',
             'value' => 'https://www.facebook.com',
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sInstagramUrl',
             'type' => 'str',
             'value' => 'https://instagram.com',
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sTwitterUrl',
             'type' => 'str',
             'value' => 'https://twitter.com',
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sYouTubeUrl',
             'type' => 'str',
             'value' => 'https://www.youtube.com',
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sPaymentIcons',
             'type' => 'bool',
             'value' => 0,
-        ),
-        array(
+        ],
+        [
             'group' => 'footer',
             'name' => 'sTrustBadges',
             'type' => 'bool',
             'value' => 0,
-        ),
+        ],
 
 //BILDER
-        array(
+        [
             'group' => 'images',
             'name' => 'aDetailImageSizes',
             'type' => 'aarr',
-            'value' => array(
+            'value' => [
                 'oxpic1' => '800*600',
                 'oxpic2' => '800*600',
                 'oxpic3' => '800*600',
@@ -234,127 +242,127 @@ $aTheme = array(
                 'oxpic10' => '800*600',
                 'oxpic11' => '800*600',
                 'oxpic12' => '800*600',
-            ),
-        ),
-        array(
+            ],
+        ],
+        [
             'group' => 'images',
             'name' => 'blSliderShowImageCaption',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sCatIconsize',
             'type' => 'str',
             'value' => '400*300',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sCatPromotionsize',
             'type' => 'str',
             'value' => '370*107',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sCatThumbnailsize',
             'type' => 'str',
             'value' => '1600*500',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sIconsize',
             'type' => 'str',
             'value' => '100*100',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sManufacturerIconsize',
             'type' => 'str',
             'value' => '100*100',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sManufacturerPromotionsize',
             'type' => 'str',
             'value' => '370*107',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sThumbnailsize',
             'type' => 'str',
             'value' => '500*500',
-        ),
-        array(
+        ],
+        [
             'group' => 'images',
             'name' => 'sZoomImageSize',
             'type' => 'str',
             'value' => '1200*1200',
-        ),
+        ],
 
 //LOGO
-        array(
+        [
             'group' => 'logo',
             'name' => 'sEmailLogo',
             'type' => 'str',
             'value' => '',
-        ),
-        array(
+        ],
+        [
             'group' => 'logo',
             'name' => 'sLogoFile',
             'type' => 'str',
             'value' => '',
-        ),
-        array(
+        ],
+        [
             'group' => 'logo',
             'name' => 'sLogoHeight',
             'type' => 'str',
             'value' => '38',
-        ),
-        array(
+        ],
+        [
             'group' => 'logo',
             'name' => 'sLogoWidth',
             'type' => 'str',
             'value' => '200',
-        ),
+        ],
 
 //FAVICON
 
-        array(
+        [
             'group' => 'favicons',
             'name' => 'sFavicon16File',
             'type' => 'str',
             'value' => 'favicon-16x16.png',
-        ),
-        array(
+        ],
+        [
             'group' => 'favicons',
             'name' => 'sFavicon32File',
             'type' => 'str',
             'value' => 'favicon-32x32.png',
-        ),
-        array(
+        ],
+        [
             'group' => 'favicons',
             'name' => 'aAppleTouchIcon',
             'type' => 'str',
             'value' => 'apple-icon-310x310.png',
-        ),
-        array(
+        ],
+        [
             'group' => 'favicons',
             'name' => 'sFaviconFile',
             'type' => 'str',
             'value' => 'favicon.ico',
-        ),
-        array(
+        ],
+        [
             'group' => 'favicons',
             'name' => 'sFaviconSvg',
             'type' => 'str',
             'value' => 'favicon.svg',
-        ),
-        array(
+        ],
+        [
             'group' => 'favicons',
             'name' => 'aOGImage',
             'type' => 'str',
             'value' => 'apex.png',
-        ),
+        ],
         [
             'group' => 'favicons',
             'name'  => 'sSiteManifestFile',
@@ -369,107 +377,107 @@ $aTheme = array(
         ],
 
 //ANALYTICS
-        array(
+        [
             'group' => 'googleanalytics',
             'name' => 'blGAAnonymizeIPs',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'googleanalytics',
             'name' => 'blUseGAEcommerceTracking',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'googleanalytics',
             'name' => 'blUseGAPageTracker',
             'type' => 'bool',
             'value' => 1,
-        ),
-        array(
+        ],
+        [
             'group' => 'googleanalytics',
             'name' => 'sGATrackingId',
             'type' => 'str',
             'value' => '',
-        ),
+        ],
 
 //GOOGLE
-        array(
+        [
             'group' => 'googlets',
             'name' => 'blUseGoogleTS',
             'type' => 'bool',
             'value' => 0,
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sDeliveryDaysNotOnStock',
             'type' => 'str',
             'value' => '14',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sDeliveryDaysOnStock',
             'type' => 'str',
             'value' => '6',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sGoogleShoppingAccountId',
             'type' => 'str',
             'value' => '',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sGoogleVendorId',
             'type' => 'str',
             'value' => '',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sPageLanguage',
             'type' => 'str',
             'value' => 'de_DE',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sShippingDaysNotOnStock',
             'type' => 'str',
             'value' => '5',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sShippingDaysOnStock',
             'type' => 'str',
             'value' => '3',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sShoppingCountry',
             'type' => 'str',
             'value' => 'DE',
-        ),
-        array(
+        ],
+        [
             'group' => 'googlets',
             'name' => 'sShoppingLanguage',
             'type' => 'str',
             'value' => 'de',
-        ),
+        ],
 //KONTAKT
-        array(
+        [
             'group' => 'contact',
             'name' => 'sGoogleMapsAddr',
             'type' => 'str',
             'value' => 'OXID eSales AG, Bertoldstraße 48, 79098 Freiburg',
-        ),
+        ],
 
 // BASKET
-        array(
+        [
             'group' => 'basket',
             'name' => 'sBasketNoticeListButtonFunction',
             'type' => 'select',
             'value' => 'move',
             'constraints' => 'hide|move|copy',
-        ),
-    ),
-);
+        ],
+    ]
+];
