acc970363125c0054048158a1f252f6302ee2748
marcelmanzel <marcel.manzel@oxid-esales.com>
2023-06-20 12:19:12 +0200

    OXDEV-7141: Revert cookie note js file; Implement cookie setting in template;

--- build/js/widgets/oxcookienote.js
+++
@@ -1,7 +0,0 @@
-function setCookieNote(cookieNote) {
-    if( !Cookies.get("displayedCookiesNotification") ) {
-        Cookies.set("displayedCookiesNotification", 1, { path: '/', expires: 30 });
-    } else {
-        cookieNote.remove();
-    }
-}
