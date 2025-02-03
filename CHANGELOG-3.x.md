a60574eaf04006d1c5e4aec2c7833c3755a18ddd
Cenarius <kiss.2002e@gmail.com>
2025-02-03 13:29:59 +0100

    Alpha 1 release
    # Conflicts:
    #	composer.json
    #	theme.php

--- CHANGELOG-3.x.md
+++ CHANGELOG-3.x.md
@@ -1,6 +1,6 @@
 # Change Log for OXID APEX Theme
 
-## v3.0.0 - Unreleased
+## v3.0.0-alpha.1 - 2025-02-03
 
 ### Added
 - Custom stock message for product when amount gets low [#0004401](https://bugs.oxid-esales.com/view.php?id=4401)
--- theme.php
+++ theme.php
@@ -12,7 +12,7 @@ $aTheme = [
     'title'       => 'APEX Theme',
     'description' => 'APEX - Bootstrap 5 TWIG Theme',
     'thumbnail'   => 'apex_green.svg',
-    'version'     => '2.1.0',
+    'version'     => '3.0.0-alpha.1',
     'author'      => 'Tino Favetto, c&c concepts and creations GmbH',
     'settings'    => [
 //DISPLAY
