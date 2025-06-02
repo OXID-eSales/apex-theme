1691cfa38a2bda6e72d76e38f96e04d1d18cf35b
ashraf <ashraf.boudawara@oxid-esales.com>
2025-06-02 15:52:32 +0200

    Fix basket changes item amount

--- build/js/pages/checkout/basket/changeamount.js
+++ build/js/pages/checkout/basket/changeamount.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    addBasketAmountEventlisteners();
+    addBasketAmountEventlisteners()
+    preventFormSubmission();
 }, false);
 
+const preventFormSubmission = function () {
+    var basketForm = document.getElementById("basket_form");
+    if (basketForm) {
+        basketForm.addEventListener("submit", function (e) {
+            e.preventDefault();
+        });
+    }
+}
 
 const addBasketAmountEventlisteners = function () {
     const aAmountButtons = document.querySelectorAll('.js-amount button');
