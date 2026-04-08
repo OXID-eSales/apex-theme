ef5baca5d40a8072c29a6939c009b5c9b95b274d
DaveOps <david.kiss@oxid-esales.com>
2026-04-08 12:25:21 +0200

    Release v3.1.0

--- theme.php
+++ theme.php
@@ -12,7 +12,7 @@ $aTheme = [
     'title'       => 'APEX Theme',
     'description' => 'APEX - Bootstrap 5 TWIG Theme',
     'thumbnail'   => 'apex_green.svg',
-    'version'     => '3.0.2',
+    'version'     => '3.1.0',
     'author'      => 'Tino Favetto, c&c concepts and creations GmbH',
     'settings'    => [
 //DISPLAY
